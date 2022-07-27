89eb89b7eb83b1b7a4b961808c1e507ab5afa309
Atsushi Kokubu <akokubu@gmail.com>
2022-07-27 20:48:51 +0900

    upload to github

---
+++ truffle-config.js
@@ -0,0 +1,42 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require("dotenv").config();
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*",
+    },
+    matic: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: mnemonic,
+          },
+          providerOrUrl: process.env.POLYGON_URL,
+          chainId: 80001,
+        }),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      chainId: 800001,
+    },
+  },
+  contract_directory: "./contracts",
+  compilers: {
+    solc: {
+      version: "^0.8.9",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  db: {
+    enabled: false,
+  },
+};
